d64219068b9de935704544e9eda67e03411cbda0
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2024-02-13 19:42:27 -0300

    Change example configuration to 16x9 farm plot

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,16 @@ pub mod trainer;
 use crop::Crop;
 
 fn main() {
-    let crop = Crop::builder(9, 5).empty((4, 0)).build();
+    let crop = Crop::builder(9, 16)
+        .empty((4, 4))
+        .empty((4, 5))
+        .empty((4, 6))
+        .empty((4, 7))
+        .empty((4, 8))
+        .empty((4, 9))
+        .empty((4, 10))
+        .empty((4, 11))
+        .build();
     let improved = trainer::CropTrainer::improve(
         &crop,
         5000,
@@ -14,7 +23,7 @@ fn main() {
             elite: 0.2,
             survivors: 0.5,
         },
-        40,
+        60,
     );
 
     //improved.print_rows()
